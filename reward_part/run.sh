aa38bc218302d078796c095adb5907775b9e01a7
XiangqiWang77 <163888746+XiangqiWang77@users.noreply.github.com>
2025-08-10 10:32:29 +0800

    Update run.sh

--- reward_part/run.sh
+++ reward_part/run.sh
@@ -1,6 +1,6 @@
 tmux new -s reward
 # 不要占 GPU
-export CUDA_VISIBLE_DEVICES=
+export CUDA_VISIBLE_DEVICES=0
 nohup uvicorn reward_server:app --host 0.0.0.0 --port 6009 --workers 1 --timeout-keep-alive 75 \
   > /users/xwang76/nano_rl/reward_part/reward.log 2>&1 & disown
 # Ctrl-b d 退出 tmux 会话，服务仍在
