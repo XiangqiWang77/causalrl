7f93d5ff27aeab66a4c77e42864dadb3b7760697
XiangqiWang77 <163888746+XiangqiWang77@users.noreply.github.com>
2025-09-21 10:02:02 +0800

    Update qwen3.17b.sh

--- qwen3.17b.sh
+++ qwen3.17b.sh
@@ -14,9 +14,9 @@ ray start --head --node-ip-address=0.0.0.0 --port=6378 --dashboard-host=0.0.0.0 
 
 data_dir=./data
 
-model_path=/groups/xzhang33/xwang76/qwen3-1.7b
+model_path=/path/to/qwen3-1.7b
 cur_task=BERT-QWEN17
-save_model_checkpoint=/groups/xzhang33/xwang76/train_models/$cur_task
+save_model_checkpoint=/path/to/train_models/$cur_task
 
 nohup python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
