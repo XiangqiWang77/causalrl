276ec86ce93d88c13d8a196bf4a22c8a3b0c704e
XiangqiWang77 <163888746+XiangqiWang77@users.noreply.github.com>
2025-09-21 09:18:26 +0800

    Update llama332B.sh

--- llama332B.sh
+++ llama332B.sh
@@ -26,9 +26,9 @@ nohup python3 -m verl.trainer.main_ppo \
     data.train_batch_size=32 \
     data.max_prompt_length=512 \
     data.truncation=right \
-    data.max_response_length=1560\
+    data.max_response_length=2048\
     actor_rollout_ref.model.path=$model_path \
-    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.actor.optim.lr=2e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=3\
     actor_rollout_ref.actor.use_dynamic_bsz=True \
